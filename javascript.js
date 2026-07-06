5d11e54f1c74d01d33ed11a35cb92659960a714b
hafsaIrfan2288 <hafsairfan619@gmail.com>
2026-07-06 06:57:51 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let Input =document.getElementById('input')
+let Main =document.getElementById('main')
+let Para =document.getElementById('para')
+
+function checknum(){ 
+ 
+ if (Input.value == ""){
+  Para.innerText = '⚠️Number is Required!';
+  Para.style.color='red'
+} else if (Number(Input.value) % 2 === 0){
+  Para.innerText = 'The Number is Even!';
+  Para.style.color='green'
+
+}else{
+  Para.innerText = 'The Number Is Odd!';
+  Para.style.color='green'
+  
+}
+  
+}
